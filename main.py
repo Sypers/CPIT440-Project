e4f005afec073127d40c68b5c44b77ba99186d96
Ahmad <aalahmary0032@stu.kau.edu.sa>
2022-10-16 16:12:49 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas
+from sklearn import linear_model
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    dataframe = pandas.read_csv('Real estate.csv')
+    
